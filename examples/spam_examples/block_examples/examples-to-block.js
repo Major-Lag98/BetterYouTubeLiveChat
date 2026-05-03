6378055a61f053f3390a6d87f99a4eba45fbcf37
Alden Smith <Aldensmith06@Gmail.com>
2026-05-02 17:47:28 -0700

    Add some examples

---
+++ examples/spam_examples/block_examples/examples-to-block.js
@@ -0,0 +1,11 @@
+[
+{
+    "messageId": "ChwKGkNLTFFyY2Z1bTVRREZmNVhUQWdkRy00VUt3",
+    "authorName": "@seshaW-sc05",
+    "authorId": null,
+    "authorType": "viewer",
+    "timestamp": "5:29 PM",
+    "messageText": "King Trump",
+    "node": {}
+}
+]
